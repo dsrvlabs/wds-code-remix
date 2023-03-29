5d5fdd9cd286595715cbacf8b40407c1deb2cce5
kairoski03 <kairoski03@gmail.com>
2023-03-29 12:55:46 +0900

    WEL-238 Remix Plugin Deploy Info - code refactoring

--- src/const/s3-path.ts
+++ src/const/s3-path.ts
@@ -3,7 +3,13 @@ export class S3Path {
     return 'wds-code-build';
   }
 
-  static outKey(chainName: string, chainId: string, account: string, timestamp: string) {
-    return `${chainName}/${chainId}/${account}/${timestamp}/out_${account}_${timestamp}_move.zip`;
+  static outKey(
+    chainName: string,
+    chainId: string,
+    account: string,
+    timestamp: string,
+    filetype: string,
+  ) {
+    return `${chainName}/${chainId}/${account}/${timestamp}/out_${account}_${timestamp}_${filetype}.zip`;
   }
 }
