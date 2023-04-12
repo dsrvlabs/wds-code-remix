7fd189a095e457115841ea938d2d8d9ba8fc44c4
kairoski03 <kairoski03@gmail.com>
2023-04-12 13:50:12 +0900

    WEL-266 Sui Type Parameter Form - type parameter stringify

--- src/components/sui/sui-parser.spec.ts
+++ src/components/sui/sui-parser.spec.ts
@@ -4,22 +4,22 @@
 
 // @ts-ignore
 
-import { stringifySuiVectorElementType, stringifySuiVectorType } from './sui-parser';
+import { stringifySuiVectorElementType, stringifySuiVectorType, suiTypeName } from './sui-parser';
 
 describe('Sui Parser', () => {
-  it('stringifySuiVectorType', () => {
+  it('suiTypeName', () => {
     const t = {
       Vector: {
         Vector: 'U8',
       },
     };
 
-    const str = stringifySuiVectorType(t);
+    const str = suiTypeName(t);
 
     expect(str).toBe('Vector<Vector<U8>>');
   });
 
-  it('stringifySuiVectorType Vector<Vector<0x1::string::String>>', () => {
+  it('suiTypeName Vector<Vector<0x1::string::String>>', () => {
     const t = {
       Vector: {
         Vector: {
@@ -33,7 +33,7 @@ describe('Sui Parser', () => {
       },
     };
 
-    const str = stringifySuiVectorType(t);
+    const str = suiTypeName(t);
 
     expect(str).toBe('Vector<Vector<0x1::string::String>>');
   });
@@ -67,5 +67,168 @@ describe('Sui Parser', () => {
     const str = stringifySuiVectorElementType(t);
 
     expect(str).toBe('0x1::string::String');
+  });
+
+  it('stringifySuiVectorElementType Vector<Vector<0x2::coin::TreasuryCap<0x9f375eded10883abdb3e93073335f4078397cdc456dd766f8baddc0fc1a6b6e7::managed::MANAGED>>>', () => {
+    const t = {
+      Vector: {
+        Vector: {
+          Struct: {
+            address: '0x2',
+            module: 'coin',
+            name: 'TreasuryCap',
+            typeArguments: [
+              {
+                Struct: {
+                  address: '0x9f375eded10883abdb3e93073335f4078397cdc456dd766f8baddc0fc1a6b6e7',
+                  module: 'managed',
+                  name: 'MANAGED',
+                  typeArguments: [],
+                },
+              },
+            ],
+          },
+        },
+      },
+    };
+
+    const str = stringifySuiVectorElementType(t);
+
+    expect(str).toBe(
+      '0x2::coin::TreasuryCap<0x9f375eded10883abdb3e93073335f4078397cdc456dd766f8baddc0fc1a6b6e7::managed::MANAGED>',
+    );
+  });
+
+  it('suiTypeName type parameter', () => {
+    const t = {
+      MutableReference: {
+        Struct: {
+          address: '0x2',
+          module: 'coin',
+          name: 'TreasuryCap',
+          typeArguments: [
+            {
+              Struct: {
+                address: '0x9f375eded10883abdb3e93073335f4078397cdc456dd766f8baddc0fc1a6b6e7',
+                module: 'managed',
+                name: 'MANAGED',
+                typeArguments: [],
+              },
+            },
+          ],
+        },
+      },
+    };
+
+    const str = suiTypeName(t);
+    expect(str).toBe(
+      '0x2::coin::TreasuryCap<0x9f375eded10883abdb3e93073335f4078397cdc456dd766f8baddc0fc1a6b6e7::managed::MANAGED>',
+    );
+  });
+
+  it('suiTypeName vector with type parameter', () => {
+    const t = {
+      Vector: {
+        Struct: {
+          address: '0x2',
+          module: 'coin',
+          name: 'TreasuryCap',
+          typeArguments: [
+            {
+              Struct: {
+                address: '0x9f375eded10883abdb3e93073335f4078397cdc456dd766f8baddc0fc1a6b6e7',
+                module: 'managed',
+                name: 'MANAGED',
+                typeArguments: [],
+              },
+            },
+          ],
+        },
+      },
+    };
+
+    const str = suiTypeName(t);
+    expect(str).toBe(
+      'Vector<0x2::coin::TreasuryCap<0x9f375eded10883abdb3e93073335f4078397cdc456dd766f8baddc0fc1a6b6e7::managed::MANAGED>>',
+    );
+
+    const str2 = stringifySuiVectorType(t);
+    expect(str2).toBe(
+      'Vector<0x2::coin::TreasuryCap<0x9f375eded10883abdb3e93073335f4078397cdc456dd766f8baddc0fc1a6b6e7::managed::MANAGED>>',
+    );
+  });
+
+  it('suiTypeName vector vector with type parameter', () => {
+    const t = {
+      Vector: {
+        Vector: {
+          Struct: {
+            address: '0x2',
+            module: 'coin',
+            name: 'TreasuryCap',
+            typeArguments: [
+              {
+                Struct: {
+                  address: '0x9f375eded10883abdb3e93073335f4078397cdc456dd766f8baddc0fc1a6b6e7',
+                  module: 'managed',
+                  name: 'MANAGED',
+                  typeArguments: [],
+                },
+              },
+            ],
+          },
+        },
+      },
+    };
+    const str = suiTypeName(t);
+    expect(str).toBe(
+      'Vector<Vector<0x2::coin::TreasuryCap<0x9f375eded10883abdb3e93073335f4078397cdc456dd766f8baddc0fc1a6b6e7::managed::MANAGED>>>',
+    );
+
+    const str2 = stringifySuiVectorType(t);
+    expect(str2).toBe(
+      'Vector<Vector<0x2::coin::TreasuryCap<0x9f375eded10883abdb3e93073335f4078397cdc456dd766f8baddc0fc1a6b6e7::managed::MANAGED>>>',
+    );
+  });
+
+  it('suiTypeName vector with type parameter list', () => {
+    const t = {
+      Vector: {
+        Vector: {
+          Struct: {
+            address: '0x2',
+            module: 'coin',
+            name: 'TreasuryCap',
+            typeArguments: [
+              {
+                Struct: {
+                  address: '0x9f375eded10883abdb3e93073335f4078397cdc456dd766f8baddc0fc1a6b6e7',
+                  module: 'managed',
+                  name: 'MANAGED',
+                  typeArguments: [],
+                },
+              },
+              {
+                Struct: {
+                  address: '0x9f375eded10883abdb3e93073335f4078397cdc456dd766f8baddc0fc1a6b6e7',
+                  module: 'managed',
+                  name: 'MANAGED2',
+                  typeArguments: [],
+                },
+              },
+            ],
+          },
+        },
+      },
+    };
+    const str = suiTypeName(t);
+    expect(str).toBe(
+      'Vector<Vector<0x2::coin::TreasuryCap<0x9f375eded10883abdb3e93073335f4078397cdc456dd766f8baddc0fc1a6b6e7::managed::MANAGED, 0x9f375eded10883abdb3e93073335f4078397cdc456dd766f8baddc0fc1a6b6e7::managed::MANAGED2>>>',
+    );
+
+    const str2 = stringifySuiVectorType(t);
+    expect(str2).toBe(
+      'Vector<Vector<0x2::coin::TreasuryCap<0x9f375eded10883abdb3e93073335f4078397cdc456dd766f8baddc0fc1a6b6e7::managed::MANAGED, 0x9f375eded10883abdb3e93073335f4078397cdc456dd766f8baddc0fc1a6b6e7::managed::MANAGED2>>>',
+    );
   });
 });
--- src/components/sui/sui-parser.ts
+++ src/components/sui/sui-parser.ts
@@ -1,13 +1,83 @@
 import { log } from '../../utils/logger';
+import { SuiMoveNormalizedType } from '@mysten/sui.js/dist/types/normalized';
+
+export function suiTypeName(parameterType: SuiMoveNormalizedType): string {
+  log.info(`parameterType`, JSON.stringify(parameterType, null, 2));
+  if (typeof parameterType === 'string') {
+    return parameterType;
+  }
+
+  if (typeof parameterType === 'number') {
+    return parameterType;
+  }
+  const t: any = parameterType;
+
+  if (t.Struct) {
+    let typeArgsStr = '';
+    if (t.Struct.typeArguments.length > 0) {
+      const typeArgs = t.Struct.typeArguments.map((targ: SuiMoveNormalizedType) => {
+        return suiTypeName(targ);
+      });
+      typeArgsStr = `<${typeArgs.join(', ')}>`;
+    }
+
+    let b = `${t.Struct.address}::${t.Struct.module}::${t.Struct.name}`;
+    if (typeArgsStr) {
+      b = b + typeArgsStr;
+    }
+    return b;
+  }
+
+  if (t.Reference) {
+    let typeArgsStr = '';
+    if (t.Reference.Struct.typeArguments.length > 0) {
+      const typeArgs = t.Reference.Struct.typeArguments.map((targ: SuiMoveNormalizedType) => {
+        return suiTypeName(targ);
+      });
+      typeArgsStr = `<${typeArgs.join(', ')}>`;
+    }
+    let b = `${t.Reference.Struct.address}::${t.Reference.Struct.module}::${t.Reference.Struct.name}`;
+    if (typeArgsStr) {
+      b = b + typeArgsStr;
+    }
+    return b;
+  }
+
+  if (t.MutableReference) {
+    let typeArgsStr = '';
+    if (t.MutableReference.Struct.typeArguments.length > 0) {
+      const typeArgs = t.MutableReference.Struct.typeArguments.map(
+        (targ: SuiMoveNormalizedType) => {
+          return suiTypeName(targ);
+        },
+      );
+      typeArgsStr = `<${typeArgs.join(', ')}>`;
+    }
+    let b = `${t.MutableReference.Struct.address}::${t.MutableReference.Struct.module}::${t.MutableReference.Struct.name}`;
+    if (typeArgsStr) {
+      b = b + typeArgsStr;
+    }
+    return b;
+  }
+
+  if (t.Vector) {
+    return stringifySuiVectorType(t);
+  }
+
+  throw new Error(`suiTypeName Invalid type ${JSON.stringify(parameterType, null, 2)}`);
+}
 
 export function stringifySuiVectorType(t: any): string {
+  if (!t.Vector) {
+    throw new Error(`stringifySuiVectorType ${JSON.stringify(t, null, 2)}`);
+  }
+
   let curVal = t;
   let cnt = 0;
   while (curVal) {
     cnt++;
 
     if (curVal.Struct) {
-      curVal = curVal.Struct;
       break;
     }
 
@@ -24,8 +94,7 @@ export function stringifySuiVectorType(t: any): string {
     prefix += 'Vector<';
   }
 
-  const elTypeStr =
-    typeof curVal === 'string' ? curVal : `${curVal.address}::${curVal.module}::${curVal.name}`;
+  const elTypeStr = suiTypeName(curVal);
 
   let postfix = '';
   for (let i = 0; i < vectorCnt; i++) {
@@ -42,7 +111,6 @@ export function stringifySuiVectorElementType(t: any): string {
     cnt++;
 
     if (curVal.Struct) {
-      curVal = curVal.Struct;
       break;
     }
 
@@ -53,7 +121,5 @@ export function stringifySuiVectorElementType(t: any): string {
     curVal = curVal.Vector;
   }
 
-  return typeof curVal === 'string'
-    ? curVal
-    : `${curVal.address}::${curVal.module}::${curVal.name}`;
+  return suiTypeName(curVal);
 }
