4b603b5fab533c8dd51e17640d9d58d6b2aeb239
kairoski03 <kairoski03@gmail.com>
2024-05-22 14:10:16 +0900

    change arbitrum dev endpoint

--- src/const/endpoint.ts
+++ src/const/endpoint.ts
@@ -55,14 +55,14 @@ export const NEUTRON_COMPILER_CONSUMER_API_ENDPOINT =
 
 const ARBITRUM_COMPILER_CONSUMER_ENDPOINT_POOL = {
   local: 'ws://localhost:8000',
-  dev: 'wss://dev.neutron.compiler.welldonestudio.io',
+  dev: 'wss://dev.compiler.welldonestudio.io',
   prod: 'wss://prod.neutron.compiler.welldonestudio.io',
 };
 export const ARBITRUM_COMPILER_CONSUMER_ENDPOINT = ARBITRUM_COMPILER_CONSUMER_ENDPOINT_POOL[STAGE];
 
 const ARBITRUM_COMPILER_CONSUMER_API_ENDPOINT_POOL = {
   local: 'http://localhost:8000',
-  dev: 'https://dev.neutron.compiler.welldonestudio.io',
+  dev: 'https://dev.compiler.welldonestudio.io',
   prod: 'https://prod.neutron.compiler.welldonestudio.io',
 };
 
