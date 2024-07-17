0cbb20fc6d1aec2bdd0935a569139aeeb74820c1
altpd13 <altpd13@gmail.com>
2024-07-17 13:20:50 +0900

    fix injective sdk has no module export error

--- config-overrides.js
+++ config-overrides.js
@@ -12,6 +12,7 @@ module.exports = {
       net: false,
       http: require.resolve('http-browserify'),
       https: require.resolve('https-browserify'),
+      zlib: require.resolve('browserify-zlib'),
       assert: false,
       url: false,
       util: require.resolve('util'),
@@ -23,7 +24,27 @@ module.exports = {
         process: 'process/browser',
       }),
     );
+    config.module.rules = config.module.rules.map((rule) => {
+      if (rule.oneOf instanceof Array) {
+        return {
+          ...rule,
+          // create-react-app let every file which doesn't match to any filename test falls back to file-loader,
+          // so we need to add purs-loader before that fallback.
+          // see: https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/config/webpack.config.dev.js#L220-L236
+          oneOf: [
+            {
+              test: /\.m?js/, // fix:issue: https://github.com/webpack/webpack/issues/11467
+              resolve: {
+                fullySpecified: false,
+              },
+            },
+            ...rule.oneOf,
+          ],
+        };
+      }
 
+      return rule;
+    });
     return config;
   },
 };
